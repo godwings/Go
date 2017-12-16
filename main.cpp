34c032fd4dc8cf9544da43435f32b62e36f11058
godwings_thinkpad_desktop <godwings@outlook.com>
2017-12-16 17:24:48 +0800

    Add Play()

--- main.cpp
+++ main.cpp
@@ -1,35 +1,34 @@
-#include <iostream>
-#include "board.h"
-#include "player.h"
-#include "printer.h"
+#include "gaming.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  Game *game = new Game();
+  game->Play();
   // dummy client code for demonstration so far
 
-  int n = 19;
-  Board board = Board(n);
+  // int n = 19;
+  // Board board = Board(n);
 
 
-  Player *p1 = new Player(),
-  *p2 = new Player();
-  Printer printer = Printer();
+  // Player *p1 = new Player(),
+  // *p2 = new Player();
+  // Printer printer = Printer();
 
 
-  board.playAt(0,0, p1);
-  board.playAt(4,5, p2);
-  board.playAt(6,7, p1);
+  // board.playAt(0,0, p1);
+  // board.playAt(4,5, p2);
+  // board.playAt(6,7, p1);
 
 
-  printer.printBoard(board, p1);
-  cout << endl;
-  cout << endl;
+  // printer.printBoard(board, p1);
+  // cout << endl;
+  // cout << endl;
 
 
-  board.removeStone(0,0);
-  printer.printBoard(board, p1);
+  // board.removeStone(0,0);
+  // printer.printBoard(board, p1);
 
-  delete p1, p2;
+  // delete p1, p2;
   return 0;
 }
